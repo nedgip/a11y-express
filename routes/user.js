c3f919d37b96e030ae8966fbf3017d044eadeeda
nedgip <christopher.pigden@gmail.com>
2020-04-30 21:21:05 +1000

    Updating with user database schema

---
+++ routes/user.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+// Login page
+router.get("/login",
+  (req, res) => {
+    res.render("login");
+  };
+
+  // Register page
+  router.get('/register', (req, res) => {
+      res.render('registration')
+  })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
-const indexRouter = require("./routes/index");
-
+// EJS
 app.set("view engine", "ejs");
-
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
-
 app.use(expressLayouts);
-app.use(express.static("public"));
-app.use("/", indexRouter);
 
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+// Database
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -26,4 +26,11 @@ db.on("error", (error) => {
 db.once("open", () => {
   console.log("Connected to mongoose");
 });
+
+// Routes
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/user");
+app.use("/", indexRouter);
+app.use("/", userRouter);
+
 app.listen(process.env.PORT || 3000);
