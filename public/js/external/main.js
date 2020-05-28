dcba596bf0aee600d47ab328ea1243212c872e76
nedgip <christopher.pigden@gmail.com>
2020-05-29 08:58:42 +1000

    Updated external JS

--- public/js/external/main.js
+++ public/js/external/main.js
@@ -14,7 +14,8 @@ const editArray = [...edit];
 
 if (params.has('edit')) {
     const script = document.querySelector('#script');
-    script.innerHTML = `<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>`
+    console.log(script)
+    script.textContent = `<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>`
     editArray.forEach((element) => {
         element.classList.add('editLive');
         // element.setAttribute('contenteditable', true);
