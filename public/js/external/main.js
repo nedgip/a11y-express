7e1b741a88fbd39269908539323d8e0863fb3bfe
nedgip <christopher.pigden@gmail.com>
2020-05-28 18:14:18 +1000

    Updated external JS

--- public/js/external/main.js
+++ public/js/external/main.js
@@ -3,3 +3,8 @@ const printBtn = document.querySelector('.print')
 printBtn.addEventListener('click', () => {
     window.print()
 })
+
+const queryString = new URLSearchParams(window.location.search);
+const currentQueryString = queryString.get('currentQueryString');
+
+console.log(myParam)
