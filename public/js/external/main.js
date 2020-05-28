7a6bfffe20dc6b97db7085ebd0c66b2fe5322b05
nedgip <christopher.pigden@gmail.com>
2020-05-29 09:49:13 +1000

    Updated external JS

--- public/js/external/main.js
+++ public/js/external/main.js
@@ -17,13 +17,30 @@ if (params.has('edit')) {
     // console.log(script)
     // script.textContent = `<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>`;
     const sidebar = document.querySelector('.off-site-container');
-    sidebar.innerHTML = `<ul class="menuList">
-     <li><a href="#">About us</a></li>
-     <li><a href="#">Products</a></li>
-     <li><a href="#">Services</a></li>
-     <li><a href="#">Electronics, computers & services</a></li>
-     <li><a href="#">Home and kitchen</a></li>
-    </ul>`
+    sidebar.innerHTML = `<div>
+    <label>Client</label>
+    <input type="text" ng-model="client">
+  </div>
+  <div>
+    <label>Product</label>
+    <input type="text">
+  </div>
+<div>
+    <label>WCAG</label>
+  <select>
+    <option>WCAG 2.1</option>
+    <option>WCAG 2.0</option>
+  </select>
+  </div>
+  <div>
+    <label>Start date</label>
+    <input type="date">
+</div>
+   <div>
+    <label>End date</label>
+    <input type="date">
+</div>
+    `
 
     // editArray.forEach((element) => {
     //     element.classList.add('editLive');
