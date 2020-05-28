a94fefc615ade486929a5922b4221acaf79d552d
nedgip <christopher.pigden@gmail.com>
2020-05-29 09:42:45 +1000

    Updated external JS

--- public/js/external/main.js
+++ public/js/external/main.js
@@ -15,13 +15,22 @@ const editArray = [...edit];
 if (params.has('edit')) {
     const script = document.querySelector('#script');
     console.log(script)
-    script.textContent = `<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>`
-    editArray.forEach((element) => {
-        element.classList.add('editLive');
-        // element.setAttribute('contenteditable', true);
-        // element.setAttribute('tabindex', 0);
-        // element.setAttribute('role', 'textarea');
+    script.textContent = `<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>`;
+    const sidebar = document.querySelector('.offsite-container');
+    sidebar.innerHTML = `<ul class="menuList">
+     <li><a href="#">About us</a></li>
+     <li><a href="#">Products</a></li>
+     <li><a href="#">Services</a></li>
+     <li><a href="#">Electronics, computers & services</a></li>
+     <li><a href="#">Home and kitchen</a></li>
+    </ul>`
+
+    // editArray.forEach((element) => {
+    //     element.classList.add('editLive');
+    // element.setAttribute('contenteditable', true);
+    // element.setAttribute('tabindex', 0);
+    // element.setAttribute('role', 'textarea');
 
 
-    })
+    // })
 }
