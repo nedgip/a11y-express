917d496ba6848f20e1f777401bcd07879f0d19fe
nedgip <christopher.pigden@gmail.com>
2020-05-28 18:18:53 +1000

    Updated external JS

--- public/js/external/main.js
+++ public/js/external/main.js
@@ -1,3 +1,5 @@
+import { Collection } from "mongoose";
+
 const printBtn = document.querySelector('.print')
 
 printBtn.addEventListener('click', () => {
@@ -7,4 +9,8 @@ printBtn.addEventListener('click', () => {
 const queryString = new URLSearchParams(window.location.search);
 const currentQueryString = queryString.get('currentQueryString');
 
-console.log(currentQueryString)
+if (currentQueryString === "edit") {
+    console.log('edit')
+} else {
+    console.log('nothing to see here')
+}
