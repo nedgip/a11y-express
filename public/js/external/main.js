bea5578c16f4464277fd95e1b702aff98e23afc3
nedgip <christopher.pigden@gmail.com>
2020-05-28 23:15:56 +1000

    Updated external JS

--- public/js/external/main.js
+++ public/js/external/main.js
@@ -10,13 +10,15 @@ printBtn.addEventListener('click', () => {
 const params = new URLSearchParams(window.location.search);
 const edit = document.querySelectorAll('.editable');
 const editArray = [...edit]
+const script = document.querySelector('#script')
 
 if (params.has('edit')) {
     editArray.forEach((element) => {
         element.classList.add('editLive');
-        element.setAttribute('contenteditable', true);
-        element.setAttribute('tabindex', 0);
-        element.setAttribute('role', 'textarea');
+        // element.setAttribute('contenteditable', true);
+        // element.setAttribute('tabindex', 0);
+        // element.setAttribute('role', 'textarea');
+        script.innerHTML = '<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>'
 
     })
 }
