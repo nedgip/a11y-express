a20e6710981d0b207785d872cd412f668200b5b5
nedgip <christopher.pigden@gmail.com>
2020-05-29 13:15:32 +1000

    Updated external JS

--- public/js/external/main.js
+++ public/js/external/main.js
@@ -63,31 +63,31 @@ config.setAttribute('ng-app', "")
 // const script = document.querySelector('#script');
 // console.log(script)
 // script.textContent = `<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>`;
-const sidebar = document.querySelector('.off-site-container');
-sidebar.innerHTML = `<div>
-    <label>Client</label>
-    <input type="text" ng-model="client">
-  </div>
-  <div>
-    <label>Product</label>
-    <input ng-model="product" type="text">
-  </div>
-<div>
-    <label>WCAG</label>
-  <select ng-model="wcag">
-    <option>WCAG 2.1</option>
-    <option>WCAG 2.0</option>
-  </select>
-  </div>
-  <div>
-    <label>Start date</label>
-    <input type="date" ng-model="start-date">
-</div>
-   <div>
-    <label>End date</label>
-    <input type="date" ng-model="end-date">
-</div>
-    `
+// const sidebar = document.querySelector('.off-site-container');
+// sidebar.innerHTML = `<div>
+//     <label>Client</label>
+//     <input type="text" ng-model="client">
+//   </div>
+//   <div>
+//     <label>Product</label>
+//     <input ng-model="product" type="text">
+//   </div>
+// <div>
+//     <label>WCAG</label>
+//   <select ng-model="wcag">
+//     <option>WCAG 2.1</option>
+//     <option>WCAG 2.0</option>
+//   </select>
+//   </div>
+//   <div>
+//     <label>Start date</label>
+//     <input type="date" ng-model="start-date">
+// </div>
+//    <div>
+//     <label>End date</label>
+//     <input type="date" ng-model="end-date">
+// </div>
+//     `
 
     // editArray.forEach((element) => {
     //     element.classList.add('editLive');
