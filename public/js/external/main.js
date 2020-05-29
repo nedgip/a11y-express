ee50e858ae7112acae60daa1c2a46fd4eaab6448
nedgip <christopher.pigden@gmail.com>
2020-05-29 14:14:27 +1000

    Updated external JS

--- public/js/external/main.js
+++ public/js/external/main.js
@@ -43,9 +43,6 @@ closeSiteMenu.addEventListener("click", event => {
 
 
 // EDIT REPORT
-// const params = new URLSearchParams(window.location.search);
-// const edit = document.querySelectorAll('.editable');
-// const editArray = [...edit];
 const config = document.querySelector('.config')
 config.setAttribute('ng-app', "")
 
@@ -58,8 +55,12 @@ wcag.setAttribute('ng-model', "wcag")
 const level = document.querySelector('.level')
 level.setAttribute('ng-model', "level")
 
-const clientValue = document.querySelector('.client-hl')
-clientValue.textContent = '{{client}}'
+const clientValue = document.querySelectorAll('.client-hl')
+clientValueArray = [...clientValue]
+clientValueArray.forEach(e => {
+    e.textContent = '{{client}}'
+})
+
 const productValue = document.querySelector('.product-hl')
 productValue.textContent = '{{product}}'
 const wcagValue = document.querySelector('.wcag-hl')
@@ -67,42 +68,3 @@ wcagValue.textContent = '{{wcag}}'
 const levelValue = document.querySelector('.level-hl')
 levelValue.textContent = '{{level}}'
 
-// if (params.has('edit')) {
-// const script = document.querySelector('#script');
-// console.log(script)
-// script.textContent = `< script src = "https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js" ></ > `;
-// const sidebar = document.querySelector('.off-site-container');
-// sidebar.innerHTML = `<div>
-//     <label>Client</label>
-//     <input type="text" ng-model="client">
-//   </div>
-//   <div>
-//     <label>Product</label>
-//     <input ng-model="product" type="text">
-//   </div>
-// <div>
-//     <label>WCAG</label>
-//   <select ng-model="wcag">
-//     <option>WCAG 2.1</option>
-//     <option>WCAG 2.0</option>
-//   </select>
-//   </div>
-//   <div>
-//     <label>Start date</label>
-//     <input type="date" ng-model="start-date">
-// </div>
-//    <div>
-//     <label>End date</label>
-//     <input type="date" ng-model="end-date">
-// </div>
-//     `
-
-    // editArray.forEach((element) => {
-    //     element.classList.add('editLive');
-    // element.setAttribute('contenteditable', true);
-    // element.setAttribute('tabindex', 0);
-    // element.setAttribute('role', 'textarea');
-
-
-    // })
-// }
