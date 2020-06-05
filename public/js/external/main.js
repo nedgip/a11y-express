d6c4701c74e62ce6119512bd67aed9927af84423
nedgip <christopher.pigden@gmail.com>
2020-06-05 22:54:42 +1000

    Update js

--- public/js/external/main.js
+++ public/js/external/main.js
@@ -81,8 +81,8 @@ const gList = document.createElement('ul')
 const bList = document.createElement('ul')
 gList.classList.add('good-list')
 bList.classList.add('bad-list')
-gListHeading.insertBefore(gList, gListHeading.nextSibling)
-bListHeading.insertBefore(bList, bListHeading.nextSibling)
+gListHeading.insertAdjacentElement("afterend", gList)
+bListHeading.insertAdjacentElement("afterend", bList)
 
 // Add list item
 const addListItemBtn = document.querySelectorAll(".add");
